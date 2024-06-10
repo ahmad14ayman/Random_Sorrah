f018cb77874df0dcbb78096f610f83be6e6b9c5f
AhMad AYman Makhlouf <152763110+ahmad14ayman@users.noreply.github.com>
2024-06-10 17:38:51 +0300

    Update Range

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let ayaNumb = document.querySelector(".aya-numb");
 let SorrahLocation = document.querySelector(".location");
 let backArrow = document.querySelectorAll(".back-arrow");
 
-let min = 72 - 1 /* this one for indexing*/;
+let min = 71 - 1 /* this one for indexing*/;
 let max = 75 - 1 /* this one for indexing*/;
 
 
